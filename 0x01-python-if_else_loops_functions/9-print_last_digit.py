67163a5b1afa01f4b4564201db5281eb56092b65
edin93 <il_matador@hotmail.fr>
2019-12-10 20:03:30 +0000

    9 done

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        r = number % 10
+        print("{:d}".format(r), end="")
+        return r
+    else:
+        r = (number % -10) * -1
+        print("{:d}".format(r), end="")
+        return r
