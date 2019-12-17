3d103d4019d1d9c9dd8c5dd06c65a5bb88612c73
edin93 <il_matador@hotmail.fr>
2019-12-17 19:18:56 +0000

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    l = len(my_list)
+    if l == 0:
+        return None
+    else:
+        max = 0
+        for i in my_list:
+            if i > max:
+                max = i
+    return max
