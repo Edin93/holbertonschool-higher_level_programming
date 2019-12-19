7da0274af01224a0a2ecc1b0151c968eeff07697
edin93 <il_matador@hotmail.fr>
2019-12-19 22:14:32 +0000

    task 15 done

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    key_arrs = []
+    for k, v in a_dictionary.items():
+        if v == value:
+            key_arrs.append(k)
+    for k in key_arrs:
+        del a_dictionary[k]
+    return a_dictionary
