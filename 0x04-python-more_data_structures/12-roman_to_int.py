023c3e6ab21e26b73a1a9151f333edf2c726d309
edin93 <il_matador@hotmail.fr>
2019-12-19 21:07:16 +0000

    task 12 done

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,13 +6,12 @@ def roman_to_int(roman_string):
         return 0
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     l = len(roman_string)
-    last_char = str[l - 1]
-    last = romans[last_char]
+    last = romans[roman_string[l - 1]]
     r = last
     for i in range(l - 1):
-        char = str[l - 2 - i]
+        char = roman_string[l - 2 - i]
         current = romans[char]
-        if current > last:
+        if current < last:
             r -= current
         else:
             r+= current
