06616744540aa8107c16b8c4cfa1b8ea2af2d34a
edin93 <il_matador@hotmail.fr>
2019-12-19 19:46:59 +0000

    task 10 done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    best_key = next(iter(a_dictionary))
+    max = a_dictionary[best_key]
+    for k, v in a_dictionary.items():
+        if v > max:
+            best_key = k
+    return best_key
