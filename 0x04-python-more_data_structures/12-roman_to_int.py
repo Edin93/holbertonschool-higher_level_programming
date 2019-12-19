f777cd52b41fdc1017dc5fa274a32f2c26f019aa
edin93 <il_matador@hotmail.fr>
2019-12-19 21:10:17 +0000

    fix task 12 pep8 errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,18 +2,18 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return 0
-    elif isinstance(roman_string, str) == False:
+    elif isinstance(roman_string, str) is not True:
         return 0
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    l = len(roman_string)
-    last = romans[roman_string[l - 1]]
+    length = len(roman_string)
+    last = romans[roman_string[length - 1]]
     r = last
-    for i in range(l - 1):
-        char = roman_string[l - 2 - i]
+    for i in range(length - 1):
+        char = roman_string[length - 2 - i]
         current = romans[char]
         if current < last:
             r -= current
         else:
-            r+= current
+            r += current
         last = current
     return r
