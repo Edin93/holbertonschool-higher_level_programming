cccb296f0c5a3a9844dcd43cfdd916bf750f0f11
edin93 <il_matador@hotmail.fr>
2019-12-19 17:48:05 +0000

    task 0 of python 4th project done

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+/**
+ * print_python_list_info - prints some basic info about python lists.
+ * @p: pointer to python objet
+ * Return: nothing.
+ */
+void print_python_list_info(PyObject *p)
+{
+}
