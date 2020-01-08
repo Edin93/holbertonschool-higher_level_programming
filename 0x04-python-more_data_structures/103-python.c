7e7f3589cca242d44a649fc9c8d6ece9edb49606
edin93 <il_matador@hotmail.fr>
2020-01-08 22:05:28 +0100

    0x05. Python - Exceptions project added to repository"

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_python_list - prints basic infos about python lists.
+ * @p: pointer.
+ * Return: nothing.
+ */
+void print_python_list(PyObject *p)
+{
+}
+/**
+ * print_python_bytes - prints basic infos about python byte objects.
+ * @p: pointer.
+ * Return: nothing.
+ */
+void print_python_bytes(PyObject *p)
+{
+}
