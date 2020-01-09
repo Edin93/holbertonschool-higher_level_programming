aece5c3f9dc4d7dca9015ef45a1f4ec91a65381c
edin93 <il_matador@hotmail.fr>
2020-01-09 03:52:31 +0100

    working on the bytecode task, unfinished work

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            i > a
+        except:
+            raise
+        result = a ** b / i
+
+    result = b + a
+
+    return result
+
+
+
+from dis import dis
+dis(magic_calculation)
