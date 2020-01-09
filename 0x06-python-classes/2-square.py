6ece85061991caf96ece286aa884d230f1769d2d
edin93 <il_matador@hotmail.fr>
2020-01-09 22:07:55 +0100

    task 2 done

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square by size.
+    Attributes:
+        size (int): square size.
+    """
+
+    def __init__(self, size=0):
+        """square class constructor.
+        Parameters:
+            size (int): square size, default value is 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
