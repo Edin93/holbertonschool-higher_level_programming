39b95826e53540e09563aa67d68d5034bc1d698b
edin93 <il_matador@hotmail.fr>
2020-01-09 22:26:55 +0100

    task 4 done

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square by size.
+
+    Attributes:
+        size (int): square size.
+    """
+
+    def __init__(self, size=0):
+        """square class constructor.
+        Parameters:
+            size (int): square size, default value is 0.
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve size from class.
+
+        Returns:
+        int: the square size.
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """square size setter.
+
+        Parameters:
+        value (int): square size.
+
+        Returns:
+        int: square size.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates the square area.
+        Returns:
+        int: the current square area.
+        """
+
+        return self.__size * self.__size
