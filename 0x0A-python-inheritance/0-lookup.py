29366df4a26d33bb1129332b88873386687ee762
edin93 <il_matador@hotmail.fr>
2020-01-21 23:53:11 +0100

    task 0 pep8 fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 """This is a simple module that has only a single function lookup.
 """
 
+
 def lookup(obj):
     """
     Return the list of available attributes and methods of obj.
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module contains a single function.
+"""
+
+
+def add_attribute(obj, attr, val):
+    """
+    Add a new attribute to an object if possible.
+    """
+    
