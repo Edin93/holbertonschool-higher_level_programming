abd32001997101dd976399de193f93fe0ca1bf64
edin93 <il_matador@hotmail.fr>
2020-01-22 23:18:31 +0100

    task 10 done

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-""" Module contains a single function.
+""" This module contains a single function.
 """
 
 
 def class_to_json(obj):
+    """
+    Return the dictionary description with simple data structure for JSON obj.
+    """
+    return obj.__dict__
