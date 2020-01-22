c6353dc1ebd745305dbc69a1d8c33a8a9e75f73c
edin93 <il_matador@hotmail.fr>
2020-01-22 22:46:51 +0100

    task 9 verify argv length

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,8 +9,9 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 if os.path.isfile('add_item.json'):
     data = load_from_json_file('add_item.json')
-    for i in range(1, len(sys.argv)):
-        data.append(str(sys.argv[i]))
-    save_to_json_file(data, 'add_item.json')
+    if (len(sys.argv) >= 2):
+        for i in range(1, len(sys.argv)):
+            data.append(str(sys.argv[i]))
+        save_to_json_file(data, 'add_item.json')
 else:
     save_to_json_file([], 'add_item.json')
