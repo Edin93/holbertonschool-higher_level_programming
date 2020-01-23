12e307ba01ea83156b913baed591d471d7cae6dd
edin93 <il_matador@hotmail.fr>
2020-01-23 23:04:34 +0100

    task 100 append done

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,12 +7,14 @@ def append_after(filename="", search_string="", new_string=""):
     """
     append new string after the line containing the search string.
     """
-    with open(filename, 'r') as f:
+    with open(filename, 'r+') as f:
         data = f.readlines()
 
+    new = []
     for i in range(len(data)):
+        new.append(data[i])
         if search_string in data[i]:
-            data.insert(i+1, new_string)
+            new.append(new_string)
 
     with open(filename, 'w') as f:
-        f.writelines(data)
+        f.writelines(new)
