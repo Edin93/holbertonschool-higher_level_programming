1d69686bc45d01a4d03a25d5de625f456d2f5b45
edin93 <il_matador@hotmail.fr>
2020-01-23 23:19:22 +0100

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,12 @@ class MyList(list):
     """
     Class inherits from list and sort its content.
     """
+    def __getitem__(self, key):
+        """
+        Get item.
+        """
+        return list.__getitem__(self, key-1)
+
     def print_sorted(self):
         """
         Prints the list elements in ascending order.
