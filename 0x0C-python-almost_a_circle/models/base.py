fcc8d1ecb1ca9becb189fd345cdbe401f5c46e86
edin93 <il_matador@hotmail.fr>
2020-01-28 17:41:18 +0100

    task 15 fix condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,11 +21,12 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Returns the JSON string representation of the given argument.
         """
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
