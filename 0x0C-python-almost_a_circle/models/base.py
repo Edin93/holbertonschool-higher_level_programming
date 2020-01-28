157b64a4c7bffc30bbbf9f050fea78dc22330029
edin93 <il_matador@hotmail.fr>
2020-01-28 17:36:48 +0100

    task 15 fix condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base():
         """
         Returns the JSON string representation of the given argument.
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
