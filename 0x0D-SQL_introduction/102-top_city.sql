93294109a13fbe15ded38f967010018f34b098dc
edin93 <il_matador@hotmail.fr>
2020-03-03 01:04:52 +0100

    task 19 correct values displayed

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,6 @@
 -- Import in hbtn_0c_0 database this table dump file 102-top_city.sql
 -- displays the top 3 of cities temperature during July and August
 -- ordered by temperature (descending)
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp FROM temperatures
+WHERE (month = 7 OR month = 8)
+GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
