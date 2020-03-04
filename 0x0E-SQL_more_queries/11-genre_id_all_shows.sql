060ad09cf6c9cf4f7820682960746dc994a5bd51
edin93 <il_matador@hotmail.fr>
2020-03-04 05:14:54 +0100

    task 11 done

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,3 +3,8 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- If a show doesn’t have a genre, display NULL
 -- I can use only one SELECT statement
+SELECT tvs.title, tvsg.genre_id
+       FROM tv_shows tvs
+       LEFT JOIN tv_show_genres tvsg
+       ON tvs.id = tvsg.show_id
+       ORDER BY tvs.title ASC, tvsg.genre_id ASC;
