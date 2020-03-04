90088fc693ae22f7564295931023781ce5218004
edin93 <il_matador@hotmail.fr>
2020-03-04 05:02:37 +0100

    task 10 fix

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- lists all shows contained in the database hbtn_0d_tvshows.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- I can use only one SELECT statement
