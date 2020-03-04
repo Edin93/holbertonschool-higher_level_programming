cd36d21662841562ae808e83844bb58cb72680e2
edin93 <il_matador@hotmail.fr>
2020-03-04 04:56:49 +0100

    task 10 done

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,3 +6,8 @@
 -- Results must be sorted in ascending order by
 -- tv_shows.title and tv_show_genres.genre_id
 -- I can use only one SELECT statement.
+SELECT tvs.title, tvsg.genre_id
+       FROM tv_shows tvs
+       LEFT JOIN tv_show_genres tvsg
+       ON tvs.id = tvsg.show_id
+       ORDER BY tvs.title ASC, tvsg.genre_id ASC;
