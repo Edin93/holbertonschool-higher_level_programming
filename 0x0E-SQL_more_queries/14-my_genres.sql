6e9e8a1a23e97431695fcea5e99fab502198bd3f
edin93 <il_matador@hotmail.fr>
2020-03-04 14:15:47 +0100

    task 14 done

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,11 @@
 -- The script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 -- I can use only one SELECT statement
+SELECT tvg.name AS name
+       FROM tv_genres tvg
+       LEFT JOIN tv_show_genres tvsg
+       ON tvsg.genre_id = tvg.id
+       WHERE tvsg.show_id = 8
+       ORDER BY tvg.name ASC;
