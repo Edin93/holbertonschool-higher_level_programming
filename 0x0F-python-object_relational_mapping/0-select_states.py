2bfe8fdbe544a82a221f622897f36d5527fd6610
edin93 <il_matador@hotmail.fr>
2020-03-28 16:51:11 +0100

    task 0 fixed (not run on imprt)

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,18 +8,19 @@ import MySQLdb
 import sys
 
 
-connection = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    charset="utf8"
-)
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cursor.fetchall()
-for row in query_rows:
-    print(row)
-cursor.close()
-connection.close()
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    connection.close()
