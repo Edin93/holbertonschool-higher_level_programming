12cdd114e91bd9bf1b9460e17e0629a01a5c7870
edin93 <il_matador@hotmail.fr>
2020-03-30 01:33:45 +0100

    task 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
 Module contains a script that lists all City objects from
-the database hbtn_0e_101_usa."""
+the database hbtn_0e_101_usa.
+"""
 
 
 from sys import argv
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State, Base
+from relationship_state import City, Base
 
 
 def list_cities():
@@ -15,19 +16,17 @@ def list_cities():
     Lists all State objects, and corresponding City objects,
     contained in the database hbtn_0e_101_usa.
     """
-    arg = sys.argv
+    arg = argv
     url_base = "mysql+mysqldb://{}:{}@localhost:3306/{}"
     db_url = url_base.format(arg[1], arg[2], arg[3])
     engine = create_engine(db_url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for s in session.query(State):
-        print(s.id, ": ",  s.name, sep="")
-        for s.cities as c in session.query(City).filter(s.cities.state_id == s.id):
-            print("\t", c.id, ": ", c.name, sep="")
-    session.close()
+    for c in session.query(City).order_by(City.id.asc()):
+        sname = c.state.name
+        print(c.id, ': ', c.name, ' -> ', sname, sep='')
 
 
 if __name__ == "__main__":
-    list_city()
+    list_cities()
