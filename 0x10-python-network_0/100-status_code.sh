7bce7a7fb7108a0997fd15ebb02180f2c138ee32
edin93 <il_matador@hotmail.fr>
2020-04-09 13:57:58 +0100

    change task 100 command

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL passed as an argument & displays the response status
-curl -sI "$1" -w '%{http_code}' | tail -1
+# Sends a request to a URL passed as an argument, displays the response status
+curl -s -o /dev/null -w "%{http_code}" "$1"
