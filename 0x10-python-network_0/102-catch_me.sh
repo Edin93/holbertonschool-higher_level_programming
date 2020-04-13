282154ac49e6e1f1794944b10d578b3b85059ff2
edin93 <il_matador@hotmail.fr>
2020-04-13 10:00:21 +0100

    task 9 done

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Requests to 0.0.0.0:5000/catch_me that causes the server to respond.
-curl -L  0.0.0.0:5000/catch_me -X PUT -d user_id="98"
+curl -L  0.0.0.0:5000/catch_me -X PUT -d user_id="98" -H "Origin: HolbertonSchool"
