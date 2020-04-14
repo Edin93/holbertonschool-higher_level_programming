bd9e48df518dfc6d8addcd2f3fc3c04fb5f45d20
edin93 <il_matador@hotmail.fr>
2020-04-15 00:12:45 +0100

    task 9 done

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that takes your Github credentials (username and password) and
+uses the Github API to display your id.
+"""
+import requests
+from sys import argv
+
+un = argv[1]
+pw = argv[2]
+url = 'https://api.github.com/user'
+headers = {}
+headers['username'] = un
+headers['Authorization'] = 'token ' + pw
+r = requests.get(url, headers=headers)
+if (r.status_code == 200):
+    j = r.json()
+    print(j['id'])
+else:
+    print('None')
