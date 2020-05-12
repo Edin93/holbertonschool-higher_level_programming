5589b09ee8b7000edcb6917574321c725589bcae
edin93 <il_matador@hotmail.fr>
2020-05-12 03:54:39 +0100

    added back up file for the 102 task

---
+++ 0x13-javascript_objects_scopes_closures/backup102.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length === 5) {
+  fs.writeFile(process.argv[4], '', { flag: 'wx' }, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+      throw err;
+    }
+    if (Object.keys(data).length !== 0 && data.constructor !== Object) {
+      fs.appendFile(process.argv[4], data + '\n', (err) => {
+        if (err) {
+          throw (err);
+        }
+      });
+    }
+  });
+  fs.readFile(process.argv[3], (err, data) => {
+    if (err) {
+      throw err;
+    }
+    if (Object.keys(data).length !== 0 && data.constructor !== Object) {
+      fs.appendFile(process.argv[4], data + '\n', (err) => {
+        if (err) {
+          throw (err);
+        }
+      });
+    }
+  });
+}
