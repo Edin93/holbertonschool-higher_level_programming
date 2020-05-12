95549caef09c2e1478edf7fdbce6701aae8336cb
edin93 <il_matador@hotmail.fr>
2020-05-12 23:00:43 +0100

    task 6 done

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length === 3) {
+  const url = process.argv[2];
+  let result = {};
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const arr = JSON.parse(body);
+      for (let i = 0; i < arr.length; i++) {
+	if (result[arr[i]["userId"]] === undefined) {
+	  result[arr[i]["userId"]] = 0;
+	}
+	if (arr[i]["completed"] === true) {
+	  result[arr[i]["userId"]] += 1;
+	}
+      }
+      console.log(result);
+    }
+  });
+}
