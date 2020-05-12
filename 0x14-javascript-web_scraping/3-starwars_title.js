7f2302b8c6e4b5765b96c231e0a79f3590cc1c40
edin93 <il_matador@hotmail.fr>
2020-05-12 21:42:36 +0100

    task 3 done

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,8 +4,12 @@ const request = require('request');
 
 if (process.argv.length === 3) {
   const filmId = process.argv[2];
-  const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
-  request.get(url).on('response', function (response) {
-    console.log(response.title);
+  const url = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).title);
+    }
   });
 }
