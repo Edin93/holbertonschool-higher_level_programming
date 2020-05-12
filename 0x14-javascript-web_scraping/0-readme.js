f99602e23c3b0f54c5f7df11c332c3a650f06886
edin93 <il_matador@hotmail.fr>
2020-05-12 20:49:57 +0100

    task 0 fix semistandard error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 if (process.argv.length === 3) {
-  const data = fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  fs.readFile(process.argv[2], 'utf-8', (error, data) => {
     if (error) {
       console.log(error);
     } else {
