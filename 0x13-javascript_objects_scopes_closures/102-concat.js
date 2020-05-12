f1a543dec7fd5c30378407fbda603ed3d8bc06e9
edin93 <il_matador@hotmail.fr>
2020-05-12 05:52:41 +0100

    task 102 done

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,33 +3,7 @@
 const fs = require('fs');
 
 if (process.argv.length === 5) {
-  fs.writeFile(process.argv[4], '', 'utf8', (err) => {
-    if (err) {
-      throw err;
-    }
-  });
-  fs.readFile(process.argv[2], (err, data) => {
-    if (err) {
-      throw err;
-    }
-    if (Object.keys(data).length !== 0 && data.constructor !== Object) {
-      fs.appendFile(process.argv[4], data + '\n', (err) => {
-        if (err) {
-          throw (err);
-        }
-      });
-    }
-  });
-  fs.readFile(process.argv[3], (err, data) => {
-    if (err) {
-      throw err;
-    }
-    if (Object.keys(data).length !== 0 && data.constructor !== Object) {
-      fs.appendFile(process.argv[4], data + '\n', (err) => {
-        if (err) {
-          throw (err);
-        }
-      });
-    }
-  });
+  const file1Content = fs.readFileSync(process.argv[2]);
+  const file2Content = fs.readFileSync(process.argv[3]);
+  fs.writeFileSync(process.argv[4], file1Content + file2Content);
 }
