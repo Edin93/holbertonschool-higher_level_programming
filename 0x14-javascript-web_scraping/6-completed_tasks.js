3a0366293dbb4f8162571533e2ae16fb259bd277
edin93 <il_matador@hotmail.fr>
2020-05-12 23:03:20 +0100

    task 6 fix semistandard error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,19 +4,19 @@ const request = require('request');
 
 if (process.argv.length === 3) {
   const url = process.argv[2];
-  let result = {};
+  const result = {};
   request(url, function (error, response, body) {
     if (error) {
       console.log(error);
     } else {
       const arr = JSON.parse(body);
       for (let i = 0; i < arr.length; i++) {
-	if (result[arr[i]["userId"]] === undefined) {
-	  result[arr[i]["userId"]] = 0;
-	}
-	if (arr[i]["completed"] === true) {
-	  result[arr[i]["userId"]] += 1;
-	}
+        if (result[arr[i].userId] === undefined) {
+          result[arr[i].userId] = 0;
+        }
+        if (arr[i].completed === true) {
+          result[arr[i].userId] += 1;
+        }
       }
       console.log(result);
     }
