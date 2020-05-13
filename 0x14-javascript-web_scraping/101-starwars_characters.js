18dd25780ca1382b58f65c889d8755791e2b4998
edin93 <il_matador@hotmail.fr>
2020-05-13 02:59:51 +0100

    task 8 done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const filmId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/`;
+const url = 'https://swapi-api.hbtn.io/api/films/';
 request(url, function (err, res, body) {
   if (err) {
     console.log(err);
@@ -10,16 +10,16 @@ request(url, function (err, res, body) {
     const results = JSON.parse(body).results;
     for (let j = 0; j < results.length; j++) {
       // console.log(results[j]);
-      if (results[j].episode_id === parseInt(filmId)) {
-	for (let i = 0; i < results[j].characters.length; i++) {
-	  request(results[j].characters[i], function (err, res, body) {
+      if (results[j].url === `${url}${filmId}/`) {
+        for (let i = 0; i < results[j].characters.length; i++) {
+          request(results[j].characters[i], function (err, res, body) {
             if (err) {
               console.log(err);
             } else {
               console.log(JSON.parse(body).name);
             }
-	  });
-	}
+          });
+        }
       }
     }
   }
