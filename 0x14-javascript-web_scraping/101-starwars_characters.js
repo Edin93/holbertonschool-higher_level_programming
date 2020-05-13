53c359471db86f14719d10589a29c78769868d81
edin93 <il_matador@hotmail.fr>
2020-05-13 04:57:40 +0100

    task 101 done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,34 +3,28 @@
 const request = require('request');
 const filmId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
+function fct (arr, i) {
+  request(arr[i], function (err, res, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      const n = JSON.parse(body).name;
+      console.log(n);
+      if (i + 1 < arr.length) {
+        fct(arr, i + 1);
+      }
+    }
+  });
+}
 request(url, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
     const results = JSON.parse(body).results;
     for (let j = 0; j < results.length; j++) {
-      // console.log(results[j]);
       if (results[j].url === `${url}${filmId}/`) {
-	console.log(results[j].characters);
-	console.log('-----------------------');
-	let characters = results[j].characters.sort(function (a, b) {
-	  // let na = a.slice(0, -1).lastIndexOf('/');
-	  // na = a.slice(na + 1, -1);
-	  let na = a.slice(a.slice(0, -1).lastIndexOf('/') + 1, -1);
-	  let nb = b.slice(b.slice(0, -1).lastIndexOf('/') + 1, -1);
-	  return (parseInt(na) - parseInt(nb));
-	});
-	console.log(characters);
-	for (let i = 0; i < characters.length; i++) {
-          request(characters[i], function (err, res, body) {
-            if (err) {
-              console.log(err);
-            } else {
-	      // console.log(JSON.parse(body).url);
-              // console.log(JSON.parse(body).name);
-            }
-          });
-        }
+        const characters = results[j].characters;
+        fct(characters, 0);
       }
     }
   }
