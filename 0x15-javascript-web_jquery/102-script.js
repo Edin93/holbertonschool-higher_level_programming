a66150ff5fbf21dd9fbe602246364fcb187cd9e0
edin93 <il_matador@hotmail.fr>
2020-05-15 02:14:10 +0100

    task 12 done

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,15 @@
+const $ = window.$;
+const url = 'https://www.fourtonfish.com/hellosalut/?lang=';
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    const search = $('INPUT#language_code').val();
+    $.ajax({
+      type: 'GET',
+      url: url + search,
+      success: function (data) {
+        console.log(data);
+        $('DIV#hello').text(data.hello);
+      }
+    });
+  });
+});
